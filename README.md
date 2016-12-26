429d713e8a08a63a431f82666fbf9f8ebc1207a4
Konstantin Gorodinskiy <mail@konstantin.io>
2016-12-26 22:37:36 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ssh-connect
 🐙 simple ssh manager
+
+## Installation
+```bash
+git clone --recursive https://github.com/gko/ssh-connect
+# in your .bashrc
+source ./ssh-connect/ssh-connect.sh
+```
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2012-2016 Konstantin Gorodinskiy
---
+++ ssh-connect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ./listbox/listbox.sh
+
+ssh-history() {
+  cat ~/.bash_history | grep -E "^ssh\s" | sed -e 's/[[:space:]]*$//' | sort -u
+}
+
+ssh-connect() {
+  local hist=$(ssh-history | tr '\n' '|')
+  listbox -t "Connect:" -o "$hist" -r res
+  echo ""
+  echo "$res" >> ~/.bash_history
+  eval "$res"
+}
