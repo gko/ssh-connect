9bb86a3d8b0816769e150ebebe344f0bdc982a13
Konstantin Gorodinskii <gor.konstantin@gmail.com>
2016-12-27 07:45:44 +0100

    Update ssh-connect.sh

--- ssh-connect.sh
+++ ssh-connect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./listbox/listbox.sh
+source $(dirname "${BASH_SOURCE[0]}")/listbox/listbox.sh
 
 ssh-history() {
   cat ~/.bash_history | grep -E "^ssh\s" | sed -e 's/[[:space:]]*$//' | sort -u
